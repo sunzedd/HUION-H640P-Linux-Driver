68a28cd843c0e0436e49a3b50c7cfd97e4d1400e
Rostislav Vivcharuk <sunz89@yandex.ru>
2021-03-02 23:50:29 +0300

    Implement basic dummy pad functionality

--- src/pad.h
+++ src/pad.h
@@ -6,7 +6,32 @@
 
 #define PAD_ENDPOINT_ADDRESS 0x82
 
-static struct usb_class_driver *pad_class_driver;
+static struct usb_class_driver pad_class_driver;
+
+
+static int pad_open(struct inode *i, struct file *f) { return 0; }
+static int pad_close(struct inode *i, struct file *f) { return 0; }
+
+static
+ssize_t pad_read(struct file *f, char __user *buf, size_t cnt, loff_t *off) {
+    // Just log for now
+    LOG_INFO("call pad_read\n");
+    return 1;
+}
+
+static 
+ssize_t pad_write(struct file *f, const char __user *buf, size_t cnt, loff_t *off) {
+    // Just log for now
+    LOG_INFO("call pad_write\n");
+    return 1;
+}
+
+static struct file_operations pad_fops = {
+    .open = pad_open,
+    .release = pad_close,
+    .read = pad_read,
+    .write = pad_write
+};
 
 static
 int pad_probe(struct usb_interface *interface,
@@ -15,12 +40,24 @@ int pad_probe(struct usb_interface *interface,
     LOG_INFO("call pad_probe\n");    
     print_usb_interface_description(interface);
 
-    return 0;
+    pad_class_driver.name = "usb/huion_pad";
+    pad_class_driver.fops = &pad_fops;
+
+    int rc = usb_register_dev(interface, &pad_class_driver);    // Этот вызов создает файл устройства (интерфеса) в dev
+    if (rc < 0) {
+        LOG_ERR("usb_register_dev FAILURE\n");
+    } else { 
+        LOG_INFO("Registered Pad with a MINOR: %d\n", interface->minor);
+    }
+
+    return rc;
 }
 
 static
 void pad_disconnect(struct usb_interface *interface) {
+
     LOG_INFO("call pad_disconnect\n");
+    usb_deregister_dev(interface, &pad_class_driver);
 }
 
 #endif // __HUION_DRAWPAD_PAD__
