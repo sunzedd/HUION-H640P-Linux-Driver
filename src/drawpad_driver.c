9aad74865f65619605cc39ee0a9eb1a244178d5b
Rostislav Vivcharuk <sunz89@yandex.ru>
2021-03-02 13:44:05 +0300

    Add function to define drawpad interface type (pen/pad)

--- src/drawpad_driver.c
+++ src/drawpad_driver.c
@@ -24,7 +24,14 @@ static void drawpad_disconnect(struct usb_interface* interface);
 
 /*                         2 Level functions                                 */
 void print_usb_interface_description(const struct usb_interface *interface);
+int get_drawpad_interface_type(const struct usb_interface *interface);
 /*---------------------------------------------------------------------------*/
+
+
+enum DRAWPAD_INTERFACE {
+    PEN = 0,
+    PAD = 1
+};
 
 
 static 
@@ -59,6 +66,20 @@ int drawpad_probe(struct usb_interface *interface,
                   const struct usb_device_id *dev_id) {
 
     LOG_INFO("probe device (%04x:%04X)", dev_id->idVendor, dev_id->idProduct);
+    
+    // определить, какой именно интерфейс вызвал функцию probe? (планшет или стилус)
+    int drawpad_interface_type = get_drawpad_interface_type(interface);
+    switch (drawpad_interface_type) {
+    case PEN:
+        // pen_probe();
+        LOG_INFO("Drawpad Interface Type: PEN\n");
+        break;
+    case PAD:
+        // pad_probe();
+        LOG_INFO("Drawpad Interface Type: PAD\n");
+        break;
+    }
+
     print_usb_interface_description(interface);
     
     return 0;
@@ -67,23 +88,25 @@ int drawpad_probe(struct usb_interface *interface,
 static 
 void drawpad_disconnect(struct usb_interface* interface) {
     LOG_INFO("call drawpad_disconnect");
+
+    // дерегистрация соответствующего USB устройства.
 }
 
 static 
 int __init drawpad_init(void) {
     int rc = 0;
-    if (0 == (rc = usb_register(&drawpad_driver))) {
+    if (0 == (rc = usb_register(&drawpad_driver))) {    // регистрация ДРАЙВЕРА в подстистеме USB.
         LOG_INFO("call usb_register: OK\n");
     } else {
         LOG_ERR("call usb_register: FAILED\n");
     }
 
-    return rc;
+    return rc;  
 }
 
 static 
 void __exit drawpad_exit(void) {
-    LOG_INFO("call usb_deregister\n");
+    LOG_INFO("call usb_deregister\n"); // дерегистрация ДРАЙВЕРА в подстистеме USB.
     usb_deregister(&drawpad_driver);
 }
 
@@ -95,7 +118,7 @@ module_init(drawpad_init);
 module_exit(drawpad_exit);
 
 
-// 2 level functions
+/*                       2 level functions                                  */
 void print_usb_interface_description(const struct usb_interface *interface) {
 
     struct usb_host_interface *interface_desc = interface->cur_altsetting;
@@ -112,6 +135,22 @@ void print_usb_interface_description(const struct usb_interface *interface) {
                   i, endpoint_desc->bEndpointAddress);
     }
 }
+
+int get_drawpad_interface_type(const struct usb_interface *interface) {
+    struct usb_host_interface *interface_desc = interface->cur_altsetting;
+    
+    int drawpad_interface_type = -1;
+    
+    switch (interface_desc->desc.bInterfaceNumber) {
+        case 0: drawpad_interface_type = PEN; break;
+        case 1: drawpad_interface_type = PAD; break;
+        // handle default?
+    }
+
+    return drawpad_interface_type;
+}
+
+/*---------------------------------------------------------------------------*/
 
 
 #endif // HUION_H640P_DRAWPAD_DRIVER
