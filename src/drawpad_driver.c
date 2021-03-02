b1cbf23a2725287bad39a02dab7c9d5ae23128ae
Rostislav Vivcharuk <sunz89@yandex.ru>
2021-03-02 14:12:55 +0300

    Add pad/pen_disconnect dummy functions, notes

--- src/drawpad_driver.c
+++ src/drawpad_driver.c
@@ -87,20 +87,47 @@ void drawpad_disconnect(struct usb_interface* interface) {
 
 
 /*                             Pen functions                                 */
+#define PEN_ENDPOINT_ADDRESS 0x81
+
+static struct usb_class_driver *pen_class_driver;
+
+static
 int pen_probe(struct usb_interface *interface,
               const struct usb_device_id *dev_id) {
+
     LOG_INFO("call pen_probe\n");
     print_usb_interface_description(interface);
+
+    // class.name = "usb/drawpad_pen%d";
+    // class.fops = &pen_fops;
+    // int rc = usb_register_dev(interface, &pen_class_driver);
+
     return 0;
 }
+
+static
+void pen_disconnect(struct usb_interface *interface) {
+    // usb_deregister_dev(interface, &pen_class_driver);
+}
+
 /*---------------------------------------------------------------------------*/
 
 /*                             Pad functions                                 */
+#define PAD_ENDPOINT_ADDRESS 0x82
+
+static struct usb_class_driver *pad_class_driver;
+
+static
 int pad_probe(struct usb_interface *interface,
               const struct usb_device_id *dev_id) {
     LOG_INFO("call pad_probe\n");    
     print_usb_interface_description(interface);
     return 0;
+}
+
+static
+void pad_disconnect(struct usb_interface *interface) {
+    
 }
 /*---------------------------------------------------------------------------*/
 
