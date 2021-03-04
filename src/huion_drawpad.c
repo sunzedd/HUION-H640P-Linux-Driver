231f95eba32c21c1a343af55e7f1babc7cf519ee
Rostislav Vivcharuk <sunz89@yandex.ru>
2021-03-04 23:19:38 +0300

    Mute PAD functions calls

--- src/huion_drawpad.c
+++ src/huion_drawpad.c
@@ -67,8 +67,10 @@ int drawpad_probe(struct usb_interface *interface,
             break;
 
         case PAD:
+            /*
             drawpad_object.pad_interface = interface;
             rc = pad_probe(interface, dev_id);
+            */
             break;
     }
     
@@ -84,7 +86,7 @@ void drawpad_disconnect(struct usb_interface* interface) {
 
     switch (get_drawpad_interface_type(interface)) {
         case PEN: pen_disconnect(interface); break;
-        case PAD: pad_disconnect(interface); break;
+        case PAD: /* pad_disconnect(interface); */ break;
     }
 }
 
