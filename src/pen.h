867170729b69ee96f758d957f8dea774b151644b
Rostislav Vivcharuk <sunz89@yandex.ru>
2021-03-04 19:39:58 +0300

    Rewrite pen probe function in proper way

--- src/pen.h
+++ src/pen.h
@@ -7,21 +7,19 @@
 
 #include "fetch_dev_info.h"
 
-#define PEN_USB_PACKET_SIZE     0x0040  // 64 bytes
 
-typedef struct pen {
+struct pen {
     char    phys[32];
 
     struct usb_device   *usb_device;
     struct input_dev    *input_device;
 
     struct urb      *urb;
+
     unsigned char   *transfer_buffer;   // буфер для чтения данных из устройства.  
     unsigned int    transfer_buffer_size;
-} pen_t;
-
-
-static pen_t pen_object;
+    dma_addr_t      dma;
+};
 
 
 static
@@ -47,70 +45,81 @@ int pen_probe(struct usb_interface *interface,
 
     LOG_INFO("\tcall pen_probe\n");
     
-    int rc = 0;
+    int rc = -ENOMEM;
 
-    pen_object.usb_device = interface_to_usbdev(interface);
-    pen_object.input_device = input_allocate_device();
-    if (pen_object.input_device == NULL) {
-        LOG_ERR("\tinput_allocate_device() FAILURE\n");
-        return -1;
+    struct usb_endpoint_descriptor *endpoint = 
+        &interface->cur_altsetting->endpoint[0].desc;
+
+    struct pen *pen = kzalloc(sizeof(struct pen), GFP_KERNEL);
+    if (!pen) {
+        LOG_ERR("\tstruct pen allocation FAILURE\n");
+        return rc;
     }
 
-    usb_make_path(pen_object.usb_device,
-                  pen_object.phys, sizeof(pen_object.phys));
-    strlcat(pen_object.phys, "/input0", sizeof(pen_object.phys));
-
-    pen_object.input_device->name = "Huion H640P Pen";
-    pen_object.input_device->phys = pen_object.phys;
-    usb_to_input_id(pen_object.usb_device, &pen_object.input_device->id);
-    pen_object.input_device->dev.parent = &interface->dev;
-    LOG_INFO("\tpen usb device path: %s", pen_object.input_device->phys);
-
-    pen_object.input_device->open = pen_open;
-    pen_object.input_device->close = pen_close;
-
-    rc = input_register_device(pen_object.input_device);
-    if (rc != 0) {
-        LOG_ERR("\tinput_register_revice() FAILURE\n");
-        return -1;
+    pen->usb_device = interface_to_usbdev(interface);
+    pen->input_device = input_allocate_device();
+    if (!pen->input_device) {
+        LOG_ERR("\tinput_allocate_device FAILURE\n");
+        kfree(pen);
+        return rc;
     }
 
-    if (rc == 0) { 
-        struct usb_host_interface *intf_desc = interface->cur_altsetting;
-        struct usb_endpoint_descriptor *endpoint_desc = &(intf_desc->endpoint[0].desc); // В устройстве по одному endpoint-у на каждый интерфейс.
-        
-        pen_object.transfer_buffer_size = endpoint_desc->wMaxPacketSize;
-        pen_object.transfer_buffer = kzalloc(pen_object.transfer_buffer_size, GFP_KERNEL);
-        if (pen_object.transfer_buffer == NULL) {
-            LOG_ERR("\tpen_object.transfer_buffer allocation FAILURE\n");
-            return -1;
-        }
+    pen->transfer_buffer_size = endpoint->wMaxPacketSize;
+    pen->transfer_buffer = usb_alloc_coherent(pen->usb_device,
+                                              pen->transfer_buffer_size,
+                                              GFP_KERNEL, &pen->dma);
+    if (!pen->transfer_buffer) {
+        LOG_ERR("\ttransfer buffer allocation FAILURE\n");
+        input_free_device(pen->input_device);
+        kfree(pen);
+        return rc;
+    }
 
-        pen_object.urb = usb_alloc_urb(0, GFP_KERNEL);
-        if (pen_object.urb == NULL) { 
-            LOG_ERR("\tpen_object.urb = usb_alloc_urb() FAILURE\n");
-            return -1;
-        }
+    pen->urb = usb_alloc_urb(0, GFP_KERNEL);
+    if (!pen->urb) {
+        LOG_ERR("\tusb_alloc_urb FAILURE\n");
+        input_free_device(pen->input_device);
+        usb_free_coherent(pen->usb_device, pen->transfer_buffer_size,
+                          pen->transfer_buffer, pen->dma);
+        kfree(pen);
+        return rc;
+    }
 
-        usb_fill_int_urb(pen_object.urb, pen_object.usb_device,
-                         usb_rcvintpipe(pen_object.usb_device, endpoint_desc->bEndpointAddress),
-                         pen_object.transfer_buffer, pen_object.transfer_buffer_size,
-                         pen_irq, &pen_object,
-                         endpoint_desc->bInterval);
+    usb_make_path(pen->usb_device, pen->phys, sizeof(pen->phys));
+    strlcat(pen->phys, "/input0", sizeof(pen->phys));
 
-        LOG_INFO("\tusb_fill_int_urb settings:\n");
-        LOG_INFO("\t\tbEndpointAddress %x, wMaxPacketSize %d, bInterval %d",
-                      endpoint_desc->bEndpointAddress,
-                      pen_object.transfer_buffer_size,
-                      endpoint_desc->bInterval);
+    pen->input_device->name = "Huion H640P Pen";
+    pen->input_device->phys = pen->phys;
+    usb_to_input_id(pen->usb_device, &pen->input_device->id);
+    pen->input_device->dev.parent = &interface->dev;
 
-        rc = usb_submit_urb(pen_object.urb, GFP_ATOMIC);
-        if (rc != 0) {
-            LOG_ERR("\tusb_submit_urb(pen, ...) FAILURE\n");
-            return rc; 
-        }
+    input_set_drvdata(pen->input_device, pen);
 
-        LOG_INFO("\tpen submited URB\n");
+    pen->input_device->open = pen_open;
+    pen->input_device->close = pen_close;
+
+    int urb_pipe = usb_rcvintpipe(pen->usb_device, endpoint->bEndpointAddress);
+    usb_fill_int_urb(pen->urb, pen->usb_device, urb_pipe,
+                     pen->transfer_buffer, pen->transfer_buffer_size,
+                     pen_irq, pen,
+                     endpoint->bInterval);
+    pen->urb->transfer_dma = pen->dma;
+    
+    rc = input_register_device(pen->input_device);
+    if (rc == 0) {
+        usb_set_intfdata(interface, pen);
+    } else {
+        LOG_ERR("\tinput_register_device FAILURE\n");
+        usb_free_urb(pen->urb);
+        input_free_device(pen->input_device);
+        usb_free_coherent(pen->usb_device, pen->transfer_buffer_size,
+                          pen->transfer_buffer, pen->dma);
+        kfree(pen);
+    }
+
+    if (rc == 0) {
+        LOG_INFO("\tRegistered Pen input device\n");
+        LOG_INFO("\tPen phys: %s\n", pen->phys);
     }
 
     return rc;
@@ -118,11 +127,18 @@ int pen_probe(struct usb_interface *interface,
 
 static
 void pen_disconnect(struct usb_interface *interface) {
-
+    
     LOG_INFO("\tcall pen_disconnect\n");
 
-    input_unregister_device(pen_object.input_device);
-    usb_kill_urb(pen_object.urb);
+    struct pen *pen = usb_get_intfdata(interface);
+    if (pen) {
+        usb_kill_urb(pen->urb);
+        usb_free_urb(pen->urb);
+        input_unregister_device(pen->input_device);
+        usb_free_coherent(pen->usb_device, pen->transfer_buffer_size,
+                          pen->transfer_buffer, pen->dma);
+        kfree(pen);
+    }
 }
 
 #endif // __HUION_DRAWPAD_PEN__
