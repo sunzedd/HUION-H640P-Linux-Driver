57e286612946c3def3f8a59f7a52f5c261296b81
Rostislav Vivcharuk <sunz89@yandex.ru>
2021-03-16 14:36:39 +0300

    Move all sources into drawpad_driver.c

--- src/drawpad_driver.c
+++ src/drawpad_driver.c
@@ -1,21 +1,111 @@
-#ifndef __HUION_DRAWPAD_PAD__
-#define __HUION_DRAWPAD_PAD__
+#ifndef __DRAWPAD_DRIVER__
+#define __DRAWPAD_DRIVER__
 
+#include <linux/module.h>
 #include <linux/init.h>
 #include <linux/slab.h>
 #include <linux/usb/input.h>
 #include <linux/interrupt.h>
 
-#include "fetch_dev_info.h"
+#include "log_utils.h"
+
 
 #define MAX_PEN_PRESSURE            8191
 #define MAX_PAD_RESOLUTION_VALUE    32767
 
-#define MAX_SCREEN_X    1920
-#define MAX_SCREEN_Y    1080
+#define MAX_SCREEN_X                1920
+#define MAX_SCREEN_Y                1080
 
 #define X_FACTOR    MAX_SCREEN_X / MAX_PAD_RESOLUTION_VALUE + 1
 #define Y_FACTOR    MAX_SCREEN_Y / MAX_PAD_RESOLUTION_VALUE + 1
+
+
+#define DRIVER_NAME     "Huion H640P Driver"
+#define DRIVER_AUTHOR   "Rostislav V."
+
+#define VENDOR_ID       0x256c    // Huion Animation Co.
+#define PRODUCT_ID      0x006d    // H640P Drawpad
+
+
+static int drawpad_probe(struct usb_interface *interface,
+                         const struct usb_device_id *dev_id);
+
+static int pad_probe(struct usb_interface *interface,
+                     const struct usb_device_id *dev_id);
+
+static void drawpad_disconnect(struct usb_interface* interface);
+static void pad_disconnect(struct usb_interface* interface);
+
+
+static 
+struct usb_device_id devices_table[] = {
+    { USB_DEVICE(VENDOR_ID, PRODUCT_ID) },
+    { }
+};
+
+MODULE_DEVICE_TABLE(usb, devices_table);
+
+static 
+struct usb_driver drawpad_driver = {
+    .name = DRIVER_NAME,
+    .id_table = devices_table,
+    .probe = drawpad_probe,
+    .disconnect = drawpad_disconnect,
+};
+
+static 
+int drawpad_probe(struct usb_interface *interface, 
+                  const struct usb_device_id *dev_id) {
+
+    LOG_INFO("probe device (%04x:%04X)\n", dev_id->idVendor, dev_id->idProduct);
+    
+    int rc = -1;
+
+    struct usb_host_interface *interface_desc = interface->cur_altsetting;
+    int interface_number = interface_desc->desc.bInterfaceNumber;
+    
+    if (interface_number == 1) {
+        rc = pad_probe(interface, dev_id);
+    }
+   
+    return rc;
+}
+
+static 
+void drawpad_disconnect(struct usb_interface* interface) {
+    LOG_INFO("disconnect device\n");
+    
+    struct usb_host_interface *interface_desc = interface->cur_altsetting;
+    int interface_number = interface_desc->desc.bInterfaceNumber;
+    
+    if (interface_number == 1) {
+        pad_disconnect(interface);
+    }
+}
+
+static 
+int __init drawpad_init(void) {
+
+    int rc = usb_register(&drawpad_driver);
+    if (rc != 0) {
+        LOG_ERR("call usb_register: FAILED\n");
+    }
+
+    return rc;  
+}
+
+static 
+void __exit drawpad_exit(void) {
+    usb_deregister(&drawpad_driver);
+}
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR(DRIVER_AUTHOR);
+
+module_init(drawpad_init);
+module_exit(drawpad_exit);
+
+
 
 
 struct pad {
@@ -302,4 +392,5 @@ void pad_disconnect(struct usb_interface *interface) {
     }
 }
 
-#endif // __HUION_DRAWPAD_PAD__
+
+#endif // __DRAWPAD_DRIVER__
