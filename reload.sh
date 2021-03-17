34f8ac68e4b1d5e454255f3c9da25fac3f096898
Rostislav Vivcharuk <sunz89@yandex.ru>
2021-03-18 00:26:57 +0300

    Add reload script

--- reload.sh
+++ reload.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/bash
 
-sudo rmmod huion_drawpad
-sudo dmesg -C
-sudo insmod huion_drawpad.ko
+sudo rmmod drawpad_driver_intf_0 drawpad_driver_intf_1
+
+cd interface_0
+make
+cd ..
+cd interface_1
+make
+cd ..
+
+sudo insmod ./interface_0/src/drawpad_driver_intf_0.ko
+sudo insmod ./interface_1/src/drawpad_driver_intf_1.ko
 
